0beed440889ede2f2fa240cbd9f8e1cbc016c4b3
Steve <johnw@motif.foundation>
2022-05-05 14:03:35 +0300

    Update api.go

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -36,7 +36,7 @@ func Api(cfg *config.Config, log logger.Logger, rs resolvers.ApiResolver) http.H
 func corsOptions(cfg *config.Config) cors.Options {
 	return cors.Options{
 
-		AllowedOrigins: []string{"https://wallet.motif.foundation"}, 
+		AllowedOrigins: []string{"http://154.12.232.29"}, 
 		AllowedMethods: []string{"HEAD", "GET", "POST"},
 		AllowedHeaders: []string{"Origin", "Accept", "Content-Type", "X-Requested-With"},
 		MaxAge:         300,
